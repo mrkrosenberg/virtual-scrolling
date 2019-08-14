574abae10458be0496d6dfd17b6d48aee28c9c85
Mark <mrk.rosenberg@gmail.com>
2019-08-13 19:56:33 -0600

    adds components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
+import { BasicScrollComponent } from './components/basic-scroll/basic-scroll.component';
+
+// Modules
+import { InfiniteScrollComponent } from './components/infinite-scroll/infinite-scroll.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+
+// Angular Fire
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule} from '@angular/fire/firestore';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicScrollComponent,
+    InfiniteScrollComponent
   ],
   imports: [
     BrowserModule,
